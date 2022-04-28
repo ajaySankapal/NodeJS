5ff4510e8e6b7efd68d8ba6db1c6cc53cf921bb0
ajaySankapal <90617524+ajaySankapal@users.noreply.github.com>
2022-04-28 10:33:32 +0530

    serving files statically

--- 02-express-js/app.js
+++ 02-express-js/app.js
@@ -13,7 +13,7 @@ const path = require("path");
 // });
 
 app.use(bodyParser.urlencoded({ extended: false }));
-
+app.use(express.static(path.join(__dirname, "public")));
 //second middleware
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
@@ -48,7 +48,9 @@ app.listen(3000);
 //        \|/
 
 //express is all about middleware
-// we can use that by app.use() - this use method allow us to use middleware in our app->this method take on function which will take place with every incoming request, this function will also take three arguements - app.use((req,res,next)={
+//->you can easily add them by calling use()
+//-> middleware function handle a request and should call next() to forward the request to the next function in line or send a response.
+//-> we can use that by app.use() - this use method allow us to use middleware in our app->this method take on function which will take place with every incoming request, this function will also take three arguements - app.use((req,res,next)={
 //next here is the function which will call the next middleware
 // })
 
@@ -68,7 +70,10 @@ app.listen(3000);
 //parsing incoming request
 //by default incoming request does not parsed, we use a package called body parser to parse the incoming request
 
-//express router
+//express routing
+//-> you can filter requests by path and method.
+//-> if you filter method,paths are matched exactly otherwise, the first segment of a url is matched.
+//-> you can use the express.Router to split your routes across files elgantely
 //to minimize the complexity of the app we make folders of the routes pages
 //and import that in our app
 
@@ -78,6 +83,9 @@ app.listen(3000);
 //res.sendFile('/views/shop.html') it will not show our page, becuase / in express does not mean the folder it means the root path
 //for this we use core module 'path'
 // res.sendFile(path.join(__dirname,'../','views','shop.html'))
+//-> you are not limited to srving dummy text as  a response
+//-> you can sendFile()s to your users - eg HTML files
+//-> if  a request is directly made for a file(e.g. a .css file is requested), you can enable static serving for such files via express.static()
 
 //using a helper function for navigation
 // instead of '../' we can use '..'
